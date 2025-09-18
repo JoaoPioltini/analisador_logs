1a75b985ea1e669b88b66e4a6c5aa4db576ad302
Joao Pedro Pioltini <joaopioltini22@gmail.com>
2025-09-18 09:06:05 -0300

    status code e bytes sendo pegos

--- log_analyser_seq.c
+++ log_analyser_seq.c
@@ -5,21 +5,40 @@ https://medium.com/@future_fanatic/a-beginners-guide-to-file-handling-in-c-with-
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void){
     FILE *arquivo;
-    char linha[1000];
-    arquivo = fopen("/Users/joaopioltini/Documents/Mackenzie/Paralela/Projeto 1/access_log_large.txt", "r");
+    char *linha = NULL;
+    size_t len = 0;
+    size_t read;
+    int max_linhas = 2;
+    int count = 0;
+    arquivo = fopen("/Users/joaopioltini/Documents/Mackenzie/Paralela/Projeto 1/analisador_logs/access_log_large.txt", "r");
     if(arquivo == NULL){
         perror("Error opening file!");
-        return 1;
+        exit(EXIT_FAILURE);
     }
 
-    while(fgets(linha, sizeof(linha), arquivo) != NULL){
-        printf("%s", linha);
+    while((read = getline(&linha, &len,arquivo)) != NULL){
+        if (count == max_linhas){
+            break;
+        }
+        count++;
+        char *quote_ptr = strstr(linha , "\" ");
+        if (quote_ptr){
+            int status_code;
+            long long bytes_sent;
+            if(sscanf(quote_ptr + 2, "%d %lld", &status_code, &bytes_sent) == 2) {
+                printf("status: %d, bytes: %lld\n", status_code, bytes_sent);
+            };
+        }
     }
 
     fclose(arquivo);
+    if(linha){
+        free(linha);
+    }
     return 0;
 
 }
